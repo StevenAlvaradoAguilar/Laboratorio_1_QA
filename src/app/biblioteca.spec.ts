bf571abebd59f5d834755403ff4ab81781fd3cc4
Steven <alvaradosteven6@gmail.com>
2022-10-08 18:39:10 -0600

    initial commit

---
+++ src/app/biblioteca.spec.ts
@@ -0,0 +1,7 @@
+import { Biblioteca } from './biblioteca';
+
+describe('Biblioteca', () => {
+  it('should create an instance', () => {
+    expect(new Biblioteca()).toBeTruthy();
+  });
+});
---
+++ src/app/detalle-prestamo.spec.ts
@@ -0,0 +1,7 @@
+import { DetallePrestamo } from './detalle-prestamo';
+
+describe('DetallePrestamo', () => {
+  it('should create an instance', () => {
+    expect(new DetallePrestamo()).toBeTruthy();
+  });
+});
